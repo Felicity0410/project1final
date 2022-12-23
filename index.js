27bd5e448f57fe09b49213cf9d72c0d3a232748e
Felicity Liu <felicityliu0410@gmail.com>
2022-12-23 18:40:18 +1030

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting && entry.boundingClientRect.y > 0) {
+        entry.target.classList.add('fade-up');
+        observer.unobserve(entry.target);
+      } else {
+        entry.target.classList.remove('fade-up');
+        observer.observe(entry.target);
+      }
+    });
+  });
+  
+  const animatedElements = document.querySelectorAll('.animated-element');
+  animatedElements.forEach(animatedElement => {
+    observer.observe(animatedElement);
+  });
